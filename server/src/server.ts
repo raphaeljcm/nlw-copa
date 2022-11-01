0bd0a19d82663be402877b43f8dadaa32c79bd01
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-01 08:52:03 -0300

    chore: setting up cors, backend set up done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 
@@ -10,13 +11,18 @@ async function bootstrap() {
     logger: true,
   });
 
+  await fastify.register(cors, {
+    origin: true,
+  });
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
 
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  // the host is for working on mobile
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
