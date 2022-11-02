36f9aa6f9d10627a0fbcaa536fa583ae29478479
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-02 18:44:15 -0300

    feat: count users, pools, and guesses routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,6 +25,18 @@ async function bootstrap() {
     return { count };
   });
 
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
   fastify.post('/pools', async (request, reply) => {
     const createdPoolBody = z.object({
       title: z.string(),
