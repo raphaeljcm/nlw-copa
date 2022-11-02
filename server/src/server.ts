438f3adbdaf3e9785d2f336a0867cff714fffbbc
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-02 18:40:36 -0300

    feat: post a pool route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import ShortUniqueId from 'short-unique-id';
+import { z } from 'zod';
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -11,14 +13,37 @@ async function bootstrap() {
     logger: true,
   });
 
+  // Middlewares
   await fastify.register(cors, {
     origin: true,
   });
 
+  // Routes
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  fastify.post('/pools', async (request, reply) => {
+    const createdPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createdPoolBody.parse(request.body);
+
+    // Creating the pool code
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   // the host is for working on mobile
