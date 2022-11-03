40445ead6ff394ec2e247ef7027d9cfff713e2d4
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-03 10:04:23 -0300

    wip-feat: setting up google oauth2

--- mobile/learning.md
+++ mobile/learning.md
@@ -3,3 +3,15 @@ Help to install fonts on the project
 
 ## React Native can't from default show svg, I gotta us some libs
 I need react-native-svg and, react-native-svg-transformer. Also configure metro.config.js
+
+## Dealing with OAuth2
+I'm using Expo Auth Session to deal with the session, and
+Expo WebBrowser to deal with accessing the system's web browser and handling redirects.
+Do not forget to add a schema to app.json
+
+### A little bit of OAuth
+It is a handshake, also known as Proof of Key Code Exchange
+It's ensure that only the app that triggred the initial auth is able to sucessfully
+exchange the verification code with a JWT token.
+
+![OAuth image](/assets/OAuth.png)
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
 type UserProps = {
   name: string;
@@ -7,6 +10,7 @@ type UserProps = {
 
 type AuthContextData = {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,17 +21,35 @@ interface AuthContextProviderProps {
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '1062298532996-sgrkbemjrfm7jd0u95k0q6hm83gnkf1m.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
+
   async function signIn() {
-    console.log('Signed In!');
+    try {
+      setIsUserLoading(true);
+
+      await promptAsync();
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
 
   return (
     <AuthContext.Provider value={{
-      signIn,
       user: {
         name: 'Raphael Marques',
         avatarUrl: 'https://github.com/raphaeljcm.png'
-      }
+      },
+      isUserLoading,
+      signIn,
      }}>
       {children}
     </AuthContext.Provider>
