3113a7f7fdd396cee17447cf14550a4815c1a66f
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 16:29:34 -0300

    feat: creating FindPool screen

---
+++ mobile/src/screens/FindPool.tsx
@@ -0,0 +1,30 @@
+import { Heading, VStack } from "native-base";
+import { Header } from "../components/Header";
+
+import Logo from '../assets/logo.svg';
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
+export function FindPool() {
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Criar novo bolão" showBackButton />
+
+      <VStack mt={8} mx={5} alignItems="center">
+        <Heading fontFamily="heading" color="white" fontSize="xl" mb={8} textAlign="center">
+          Encontre um bolão através de {'\n'}
+          seu código único
+        </Heading>
+
+        <Input
+          mb={2}
+          placeholder="Qual o código do bolão?"
+        />
+
+        <Button
+          title="BUSCAR BOLÃO"
+        />
+      </VStack>
+    </VStack>
+  )
+}
