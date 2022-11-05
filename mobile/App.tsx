fed297508c054bf1e13f9f0438068a217cad861f
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 16:20:14 -0300

    refactor: refactioring App component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,6 +6,7 @@ import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
+import { NewPool } from './src/screens/NewPool';
 
 export default function App() {
   const [fontsLoaded] = useFonts({  Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <NewPool /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
