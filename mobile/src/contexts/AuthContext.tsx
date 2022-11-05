1b3cf4a190373947d3029c221b4b8a4f3784f494
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 16:19:44 -0300

    feat: creating AuthContext

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
@@ -21,6 +21,7 @@ interface AuthContextProviderProps {
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({} as UserProps);
   const [isUserLoading, setIsUserLoading] = useState(false);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
@@ -42,12 +43,19 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
   }
 
+  async function signInWithGoogle(access_token: string) {
+    console.log('Auth Token: ', access_token);
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
   return (
     <AuthContext.Provider value={{
-      user: {
-        name: 'Raphael Marques',
-        avatarUrl: 'https://github.com/raphaeljcm.png'
-      },
+      user,
       isUserLoading,
       signIn,
      }}>
