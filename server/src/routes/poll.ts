8f371d467ade6ceb80d635f4b768c685e8332d3e
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 20:15:29 -0300

    feat: joining a poll route

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -3,6 +3,7 @@ import ShortUniqueId from 'short-unique-id';
 import { z } from 'zod';
 
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function pollRoutes(fastify: FastifyInstance) {
   fastify.get('/polls/count', async () => {
@@ -49,4 +50,63 @@ export async function pollRoutes(fastify: FastifyInstance) {
 
     return reply.status(201).send({ code });
   });
+
+  fastify.post(
+    '/polls/:id/join',
+    { onRequest: [authenticate] },
+    async (request, reply) => {
+      const joinPollBy = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPollBy.parse(request.body);
+
+      const poll = await prisma.poll.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!poll) {
+        return reply.status(404).send({
+          message: 'Poll not found',
+        });
+      }
+
+      if (poll.participants.length > 0) {
+        return reply.status(400).send({
+          message: 'You have already joined this poll',
+        });
+      }
+
+      // If this poll doesn't have a owner, make this first user
+      // trying to join the owner
+      if (!poll.ownerId) {
+        await prisma.poll.update({
+          where: {
+            id: poll.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        });
+      }
+
+      await prisma.participant.create({
+        data: {
+          pollId: poll.id,
+          userId: request.user.sub,
+        },
+      });
+
+      return reply.status(201).send();
+    },
+  );
 }
