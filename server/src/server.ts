e535a21e210de081896af79093d52114ca5bba33
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 19:50:18 -0300

    feat: jwt validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import Fastify from 'fastify';
 
+import { authRoutes } from './routes/auth';
 import { guessRoutes } from './routes/guess';
 import { pollRoutes } from './routes/poll';
 import { userRoutes } from './routes/user';
@@ -18,6 +20,11 @@ async function bootstrap() {
   await fastify.register(pollRoutes);
   await fastify.register(userRoutes);
   await fastify.register(guessRoutes);
+  await fastify.register(authRoutes);
+
+  await fastify.register(jwt, {
+    secret: process.env.JWT_SECRET!,
+  });
 
   // the host is for working on mobile
   await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
