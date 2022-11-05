1ad1ee2dac252e5d1f40c329ed6d81af88c6bed6
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-05 20:06:06 -0300

    feat: create pool with jwt

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -22,12 +22,30 @@ export async function pollRoutes(fastify: FastifyInstance) {
     const generate = new ShortUniqueId({ length: 6 });
     const code = String(generate()).toUpperCase();
 
-    await prisma.poll.create({
-      data: {
-        title,
-        code,
-      },
-    });
+    try {
+      await request.jwtVerify();
+
+      await prisma.poll.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          participants: {
+            create: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+    } catch {
+      await prisma.poll.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
 
     return reply.status(201).send({ code });
   });
