03841e30b5ff049c2a821224aeed4bb0a4766e13
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-06 20:29:15 -0300

    feat: find poll

--- mobile/src/screens/FindPoll.tsx
+++ mobile/src/screens/FindPoll.tsx
@@ -1,13 +1,50 @@
-import { Heading, VStack } from "native-base";
+import { Heading, useToast, VStack } from "native-base";
 import { Header } from "../components/Header";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from "react";
+import { getToast, TOAST_SUCCESS_KEY } from "../utils/toast";
+import { api } from "../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 export function FindPoll() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState("");
+
+  const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function handleJoinPoll() {
+    try {
+      setIsLoading(true);
+
+      if (!code.trim()) {
+        toast.show(getToast('Informe o código'));
+      }
+
+      await api.post('/polls/join', { code });
+      setIsLoading(false);
+
+      toast.show(getToast('Você entrou no bolão com sucesso!', TOAST_SUCCESS_KEY));
+      navigate('polls');
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+
+      if (error.response?.data?.message === 'Poll not found') {
+        toast.show(getToast('Não foi possível encontrar o bolão'));
+      }
+
+      if (error.response?.data?.message === 'You have already joined this poll') {
+        toast.show(getToast('Você já está nesse bolão'));
+      }
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title="Criar novo bolão" showBackButton />
+      <Header title="Buscar por código" showBackButton />
 
       <VStack mt={8} mx={5} alignItems="center">
         <Heading fontFamily="heading" color="white" fontSize="xl" mb={8} textAlign="center">
@@ -18,10 +55,15 @@ export function FindPoll() {
         <Input
           mb={2}
           placeholder="Qual o código do bolão?"
+          autoCapitalize="characters"
+          onChangeText={setCode}
+          value={code}
         />
 
         <Button
           title="BUSCAR BOLÃO"
+          isLoading={isLoading}
+          onPress={handleJoinPoll}
         />
       </VStack>
     </VStack>
