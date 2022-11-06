d80be4d1a4943ef8d3e847c4c7b8fdbaf6a96567
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-06 16:01:02 -0300

    type: navigation type

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      newPoll: undefined;
+      polls: undefined;
+      findPoll: undefined;
+      details: {
+        id: string;
+      }
+    }
+  }
+}
